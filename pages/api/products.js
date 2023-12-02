a444dd2352f9c08a0bb2ea838997c5ef693cba09
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-12-02 17:25:29 +0530

    delete product api and ui full functionality

--- pages/api/products.js
+++ pages/api/products.js
@@ -30,4 +30,11 @@ export default  async function handle(req,res){
         res.json('Updated ')
         
     }
+
+    if(method==='DELETE'){
+        if(req.query?.id){
+            await Product.deleteOne({_id:req.query.id})
+            res.json('deleted')
+        }
+    }
 }
---
+++ pages/products/delete/[...id].jsx
@@ -0,0 +1,51 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import {useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function DeleteProduct(){
+    const router = useRouter();
+    const{id} = router.query;
+    const[productInfo,setProductInfo] = useState()
+   useEffect(()=>{
+    if(id){
+
+        axios.get('/api/products?id='+id).then(response=>{
+            setProductInfo(response.data)
+
+        })
+
+    }
+   },[id])
+
+
+
+   function goBack(){
+    router.push('/products')
+   }
+
+   const deleteProduct = async()=>{
+    axios.delete('/api/products?id='+id)
+    router.push('/products')
+
+   }
+    return(
+        <Layout>
+            <div className="mt-52">
+           <h1 className="text-center my-6"> Do you want to delete "{productInfo?.title}"?</h1>
+           
+           <div className="flex gap-3 justify-center ">
+            <button 
+            className="btn-red"
+            onClick={deleteProduct}>Yes
+            </button>
+
+            <button 
+            onClick={goBack} 
+            className="btn-default">No</button>
+            
+            </div>
+            </div>
+        </Layout>
+    )
+}
